2ae8bf93fef3fafcc8f76902404bf4f8ad4c5ee2
wallback <wallback@yandex.ru>
2015-04-29 01:57:57 +0300

    новый день

--- hearth.lua
+++ hearth.lua
@@ -125,13 +125,13 @@ end
 --------------------------------------------------------------------------------------------------
 function ladder:hunt1 ()
   local my="Hunt face"
-  self:mage (my,1,0,1)
+  self:mage (my,1,0,1,1)
   self:warior (my,1,0)
   self:shaman (my,0)
   self:druid (my)
-  self:priest (my,1)
-  self:hunter (my,1,0)
-  self:paladin (my,1)
+  self:priest (my,1,1)
+  self:hunter (my,1,0,1)
+  self:paladin (my,1,0)
   self:rogue (my)
   self:warlock (my)
 end
@@ -167,13 +167,27 @@ function ladder:lock1 ()
   self:mage (my)
   self:warior (my,1,1)
   self:shaman (my)
-  self:druid (my,1)
-  self:priest (my,0)
+  self:druid (my,1,0)
+  self:priest (my,0,1)
+  self:hunter (my,1,0,1)
+  self:paladin (my)
+  self:rogue (my,1)
+  self:warlock (my)
+end
+--------------------------------------------------------------------------------------------------
+function ladder:hunt2 ()
+  local my="Hunt beast"
+  self:mage (my,1,0,0)
+  self:warior (my,0)
+  self:shaman (my,1)
+  self:druid (my,1,0)
+  self:priest (my)
   self:hunter (my,1,0)
   self:paladin (my)
   self:rogue (my)
-  self:warlock (my)
+  self:warlock (my,1)
 end
+--------------------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------
 ladder:sham1()
 ladder:sham2()
@@ -181,5 +195,6 @@ ladder:hunt1()
 ladder:druid1()
 ladder:druid2()
 ladder:lock1()
+ladder:hunt2()
 
 ladder:print1()
