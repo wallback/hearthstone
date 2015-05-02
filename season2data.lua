9d9b3a142db11f6d7b2f6669d5ee58f0e0fcdd7d
wallback <wallback@yandex.ru>
2015-05-02 16:43:45 +0300

    Пытаемся фейсхантом ворваться хоть куда-то

--- season2data.lua
+++ season2data.lua
@@ -1,14 +1,14 @@
 function ladder:hunt1 ()
   local my="Hunt face"
-  self:mage (my)
+  self:mage (my,1,0,1)
   self:warior (my)
   self:shaman (my)
-  self:druid (my,1)
+  self:druid (my,1,1)
   self:priest (my)
-  self:hunter (my)
-  self:paladin (my)
+  self:hunter (my,1,0)
+  self:paladin (my,1)
   self:rogue (my)
-  self:warlock (my)
+  self:warlock (my,1)
 end
 
 ladder:hunt1()
