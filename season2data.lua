ac6cef1a8c0869d3bc3c0b90facd487535db86f5
wallback <wallback@yandex.ru>
2015-05-02 21:03:23 +0300

    Не так уж он и крут этот фейс-хант

--- season2data.lua
+++ season2data.lua
@@ -1,14 +1,14 @@
 function ladder:hunt1 ()
   local my="Hunt face"
-  self:mage (my,1,0,1)
-  self:warior (my)
+  self:mage (my,1,0,1,0,0,0,1)
+  self:warior (my,0)
   self:shaman (my)
-  self:druid (my,1,1)
-  self:priest (my)
-  self:hunter (my,1,0)
-  self:paladin (my,1)
-  self:rogue (my)
-  self:warlock (my,1)
+  self:druid (my,1,1,1,0,0,0)
+  self:priest (my,1)
+  self:hunter (my,1,0,1,0,1)
+  self:paladin (my,1,1)
+  self:rogue (my,1,0)
+  self:warlock (my,1,1,0,1,1)
 end
 
 ladder:hunt1()
