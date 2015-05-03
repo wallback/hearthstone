ae2ad40dabce39121bbff2eecc5c4e528ca6d2d4
wallback <wallback@yandex.ru>
2015-05-03 10:03:43 +0300

    Начали шаманом баловаться. Весьма успешно. 15 левел наш.

--- season2data.lua
+++ season2data.lua
@@ -10,5 +10,33 @@ function ladder:hunt1 ()
   self:rogue (my,1,0)
   self:warlock (my,1,1,0,1,1)
 end
+--------------------------------------------------------------------------------------------------
+function ladder:sham1 ()
+  local my="Shaman mech"
+  self:mage (my)
+  self:warior (my)
+  self:shaman (my)
+  self:druid (my)
+  self:priest (my)
+  self:hunter (my)
+  self:paladin (my)
+  self:rogue (my)
+  self:warlock (my)
+end
+--------------------------------------------------------------------------------------------------
+function ladder:sham2 ()
+  local my="Shaman blood"
+  self:mage (my,0,1)
+  self:warior (my,0)
+  self:shaman (my)
+  self:druid (my)
+  self:priest (my,1)
+  self:hunter (my,1)
+  self:paladin (my,1)
+  self:rogue (my)
+  self:warlock (my,0,1)
+end
+--------------------------------------------------------------------------------------------------
 
 ladder:hunt1()
+ladder:sham2()
