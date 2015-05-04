5aa0e96d7d02629bd31b354f960e0988ca2b4abc
wallback <wallback@yandex.ru>
2015-05-04 22:55:00 +0300

    111

--- season2data.lua
+++ season2data.lua
@@ -26,17 +26,43 @@ end
 --------------------------------------------------------------------------------------------------
 function ladder:sham2 ()
   local my="Shaman blood"
-  self:mage (my,0,1,1,0,0,1)
+  self:mage (my,0,1,1,0,0,1,0,0)
   self:warior (my,0,1,0)
   self:shaman (my)
   self:druid (my,1)
-  self:priest (my,1)
-  self:hunter (my,1,1,0,1)
-  self:paladin (my,1,1,0)
+  self:priest (my,1,1)
+  self:hunter (my,1,1,0,1,1,0)
+  self:paladin (my,1,1,0,0)
   self:rogue (my,0,1)
   self:warlock (my,0,1,1,0,1)
 end
 --------------------------------------------------------------------------------------------------
+function ladder:sham3 ()
+  local my="Shaman agro"
+  self:mage (my,0,0)
+  self:warior (my,0)
+  self:shaman (my)
+  self:druid (my)
+  self:priest (my)
+  self:hunter (my)
+  self:paladin (my)
+  self:rogue (my)
+  self:warlock (my)
+end
+--------------------------------------------------------------------------------------------------
+function ladder:druid1 ()
+  local my="Druid agro"
+  self:mage (my,1,0)
+  self:warior (my)
+  self:shaman (my)
+  self:druid (my,0,1,0)
+  self:priest (my,0)
+  self:hunter (my)
+  self:paladin (my)
+  self:rogue (my)
+  self:warlock (my,0)
+end
 
 ladder:hunt1()
 ladder:sham2()
+ladder:druid1()
