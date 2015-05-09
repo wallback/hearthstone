9a709017fb7bbfb9cf922b7e188f4ff603ab555d
wallback <wallback@yandex.ru>
2015-05-09 20:25:15 +0300

    шаман пытается выруливать мехас

--- season2data.lua
+++ season2data.lua
@@ -13,15 +13,15 @@ end
 --------------------------------------------------------------------------------------------------
 function ladder:sham1 ()
   local my="Shaman mech"
-  self:mage (my,1,1)
-  self:warior (my,1,0)
-  self:shaman (my)
+  self:mage (my,1,1,1)
+  self:warior (my,1,0,1)
+  self:shaman (my,0)
   self:druid (my)
-  self:priest (my)
+  self:priest (my,1)
   self:hunter (my)
   self:paladin (my)
   self:rogue (my)
-  self:warlock (my,0,1)
+  self:warlock (my,0,1,0)
 end
 --------------------------------------------------------------------------------------------------
 function ladder:sham2 ()
@@ -35,19 +35,6 @@ function ladder:sham2 ()
   self:paladin (my,1,1,0,0)
   self:rogue (my,0,1)
   self:warlock (my,0,1,1,0,1)
-end
---------------------------------------------------------------------------------------------------
-function ladder:sham3 ()
-  local my="Shaman agro"
-  self:mage (my,0,0)
-  self:warior (my,0)
-  self:shaman (my)
-  self:druid (my)
-  self:priest (my)
-  self:hunter (my)
-  self:paladin (my)
-  self:rogue (my)
-  self:warlock (my)
 end
 --------------------------------------------------------------------------------------------------
 function ladder:druid1 ()
@@ -75,9 +62,10 @@ function ladder:mage1 ()
   self:rogue (my,1)
   self:warlock (my,1,1,0,1,0)
 end
-
+--------------------------------------------------------------------------------------------------
 ladder:hunt1()
 ladder:sham2()
 ladder:druid1()
 ladder:mage1()
 ladder:sham1()
+--------------------------------------------------------------------------------------------------
