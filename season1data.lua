69d35b3c9a9bc2294a8af85e64d1875c23c5532b
wallback <wallback@yandex.ru>
2015-05-17 16:38:20 +0300

    Переделал первый сезон на новый лад

--- season1data.lua
+++ season1data.lua
@@ -1,116 +1,66 @@
-function ladder:sham1 ()
-  local my="Shaman mech"
-  self:mage (my,0,1)
-  self:warrior (my,1,1)
-  self:shaman (my)
-  self:druid (my)
-  self:priest (my,0,0,1,0)
-  self:hunter (my,1,0,0)
-  self:paladin (my,0,1,0)
-  self:rogue (my,1,1)
-  self:warlock (my,1,1)
-end
 --------------------------------------------------------------------------------------------------
-function ladder:sham2 ()
-  local my="Shaman blood"
-  self:mage (my,1,0,0,0,1)
-  self:warrior (my)
-  self:shaman (my,1)
-  self:druid (my)
-  self:priest (my)
-  self:hunter (my,0)
-  self:paladin (my)
-  self:rogue (my)
-  self:warlock (my,1,1,1)
-end
---21-93-49
---8-937-449-39-497
---аникин
---8-951-358-58-10
+IM="Shaman mech"
+  Mage (0,1)
+  Warrior (1,1)
+  Priest (0,0,1,0)
+  Hunter (1,0,0)
+  Paladin (0,1,0)
+  Rogue (1,1)
+  Warlock (1,1)
 --------------------------------------------------------------------------------------------------
-function ladder:hunt1 ()
-  local my="Hunt face"
-  self:mage (my,1,0,1,1,0,1,0,1,0,0,0,0,0)
-  self:warrior (my,1,0,0)
-  self:shaman (my,0,1,1)
-  self:druid (my,1,0,1)
-  self:priest (my,1,1)
-  self:hunter (my,1,0,1,1,0,1,1,0,0,1)
-  self:paladin (my,1,0,1,1,0,0,1,0,0,1)
-  self:rogue (my,0,1,1,1,1,1,0,1)
-  self:warlock (my,0,1,0,1,0,1)
-end
+IM="Shaman blood"
+  Mage (1,0,0,0,1)
+  Shaman (1)
+  Hunter (0)
+  Warlock (1,1,1)
 --------------------------------------------------------------------------------------------------
-function ladder:druid1 ()
-  local my="Druid agro"
-  self:mage (my,1,1)
-  self:warrior (my,0,1)
-  self:shaman (my)
-  self:druid (my,1,1)
-  self:priest (my)
-  self:hunter (my,0,0,0)
-  self:paladin (my,1,0,1,0,0)
-  self:rogue (my)
-  self:warlock (my,0,0,0,0)
-end
+IM="Hunt face"
+  Mage (1,0,1,1,0,1,0,1,0,0,0,0,0)
+  Warrior (1,0,0)
+  Shaman (0,1,1)
+  Druid (1,0,1)
+  Priest (1,1)
+  Hunter (1,0,1,1,0,1,1,0,0,1)
+  Paladin (1,0,1,1,0,0,1,0,0,1)
+  Rogue (0,1,1,1,1,1,0,1)
+  Warlock (0,1,0,1,0,1)
 --------------------------------------------------------------------------------------------------
-function ladder:druid2 ()
-  local my="Druid tank"
-  self:mage (my,0)
-  self:warrior (my)
-  self:shaman (my,1)
-  self:druid (my)
-  self:priest (my,0,1)
-  self:hunter (my,0,1,1)
-  self:paladin (my,0,0,0)
-  self:rogue (my)
-  self:warlock (my,1,0,1)
-end
+IM="Druid agro"
+  Mage (1,1)
+  Warrior (0,1)
+  Druid (1,1)
+  Hunter (0,0,0)
+  Paladin (1,0,1,0,0)
+  Warlock (0,0,0,0)
 --------------------------------------------------------------------------------------------------
-function ladder:lock1 ()
-  local my="Warlock demon"
-  self:mage (my,0)
-  self:warrior (my,1,1)
-  self:shaman (my)
-  self:druid (my,1,0)
-  self:priest (my,0,1)
-  self:hunter (my,1,0,1,0)
-  self:paladin (my,0)
-  self:rogue (my,1)
-  self:warlock (my,1,0,0)
-end
+IM="Druid tank"
+  Mage (0)
+  Shaman (1)
+  Priest (0,1)
+  Hunter (0,1,1)
+  Paladin (0,0,0)
+  Warlock (1,0,1)
 --------------------------------------------------------------------------------------------------
-function ladder:hunt2 ()
-  local my="Hunt beast"
-  self:mage (my,1,0,0)
-  self:warrior (my,0)
-  self:shaman (my,1)
-  self:druid (my,1,0)
-  self:priest (my)
-  self:hunter (my,1,0)
-  self:paladin (my)
-  self:rogue (my)
-  self:warlock (my,1)
-end
+IM="Warlock demon"
+  Mage (0)
+  Warrior (1,1)
+  Druid (1,0)
+  Priest (0,1)
+  Hunter (1,0,1,0)
+  Paladin (0)
+  Rogue (1)
+  Warlock (1,0,0)
 --------------------------------------------------------------------------------------------------
-function ladder:priest1 ()
-  local my="Priest antirush"
-  self:mage (my)
-  self:warrior (my)
-  self:shaman (my)
-  self:druid (my)
-  self:priest (my,0)
-  self:hunter (my,1,0)
-  self:paladin (my,0)
-  self:rogue (my)
-  self:warlock (my)
-end
+IM="Hunt beast"
+  Mage (1,0,0)
+  Warrior (0)
+  Shaman (1)
+  Druid (1,0)
+  Hunter (1,0)
+  Warlock (1)
 --------------------------------------------------------------------------------------------------
-ladder:sham1()
-ladder:sham2()
-ladder:hunt1()
-ladder:druid1()
-ladder:druid2()
-ladder:lock1()
-ladder:hunt2()
-ladder:priest1()
+IM="Priest antirush"
+  Priest (0)
+  Hunter (1,0)
+  Paladin (0)
+--------------------------------------------------------------------------------------------------
